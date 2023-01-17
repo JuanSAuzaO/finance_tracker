e33c4078d9ada37f9fac9000e87db9d9b5c8ce73
JuanSAuzaO <sebastianauza@hotmail.com>
2023-01-17 09:33:57 -0500

    Add the feature of tracking stocks from the user portfolio

---
+++ app/controllers/user_stocks_controller.rb
@@ -0,0 +1,13 @@
+class UserStocksController < ApplicationController
+
+  def create
+    stock = Stock.check_db(params[:stock])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:stock])
+      stock.save
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was succesfuly added to your portfolio"
+    redirect_to my_portfolio_path
+  end
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -11,5 +15,9 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
